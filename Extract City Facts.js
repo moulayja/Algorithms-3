4bdef607bb66e0683815a3918d9e5361566404fe
Jamal <moulayjam@gmail.com>
2020-05-08 02:10:24 -0400

    Extract City Facts

--- Extract City Facts.js
+++ Extract City Facts.js
@@ -1,4 +1,5 @@
 //Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
 
 function cityFacts(city) {
-	return `${city.name} has a population of ${city.population} and is situated in ${city.continent}`;
+    return `${city.name} has a population of ${city.population} and is situated in ${city.continent}`
+}
