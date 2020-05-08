bf74cbb49b563cfbe1a510f792558654e5b6ab60
Jamal <moulayjam@gmail.com>
2020-05-08 02:06:31 -0400

    1st commit

---
+++ Extract City Facts.js
@@ -0,0 +1,2 @@
+//Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
+
